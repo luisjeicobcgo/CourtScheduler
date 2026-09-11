88f2f147d9ae4100df0a4630d7f7a5a41d77b366
Halili, Ronazld R. <ronaldhalili05@gmail.com>
2026-09-11 14:19:44 +0800

    all done for minipeta 2

---
+++ app/src/test/java/com/example/courtscheduler/quarter2/almonteminipeta.java
@@ -0,0 +1,20 @@
+package com.example.courtscheduler.quarter2;
+import org.junit.Test;
+public class almonteminipeta {
+    @Test
+    public void printMYprofile(){
+        String myName = "Stephen";
+        String petname1 = "nguso";
+        String petname2 = "ayat";
+        String favfood = "kare-kare";
+        int myAge = 17;
+
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have 2 wonderful pets named " + petname1 + " and " + petname2 + ".");
+        System.out.println("If I could eat " + favfood + " everyday!");
+
+    }
+
+}
