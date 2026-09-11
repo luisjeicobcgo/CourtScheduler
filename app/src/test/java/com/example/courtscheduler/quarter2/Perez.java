d1d6dbdc67af811c4b71958932bed0ca83257d5c
aldemer-perez <aldemerjustwater@gmail.com>
2026-09-11 16:01:06 +0800

    All done Debugged and checked for testing purposes of the mini peta

---
+++ app/src/test/java/com/example/courtscheduler/quarter2/Perez.java
@@ -0,0 +1,18 @@
+package com.example.courtscheduler.quarter2;
+
+ import org.junit.Test;
+
+public class Perez {
+     @Test
+    public void printMyProfile() {
+         String myName = "Aldemer";
+         String petName = "Milky";
+         String favFood = "Carbonara";
+         int myAge = 17;
+
+         System.out.println("--- MY DIGITAL PROFILE ---");
+         System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+         System.out.println("I have a wonderful pet named " + petName + ".");
+         System.out.println("If I could, I would eat " + favFood + " every single day!");
+     }
+}
