257f7e0aed5bed25e192eb22dd08279911cf5d17
dorendez17 <gwynvanzuela@gmail.com>
2026-09-18 08:36:14 +0800

    DIGITAL PROFILE PENOLIAR

---
+++ app/src/test/java/com/example/courtscheduler/quarter2/Penoliarminipeta.java
@@ -0,0 +1,23 @@
+package com.example.courtscheduler.quarter2;
+
+import org.junit.Test;
+
+public class Penoliarminipeta {
+    @Test
+    public void printMYprofile(){
+        String myName = "Kyle";
+        String petname1 = "Tofu";
+        String petname2 = "Leng";
+        String favfood = "Sinigang";
+        int myAge = 17;
+
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have 2 wonderful pets named " + petname1 + " and " + petname2 + ".");
+        System.out.println("If I could eat " + favfood + " everyday!");
+
+    }
+
+}
+
