5874cdef06794086d3f020443ec5d2e195ee35d5
nep1009 <phenalmonte@gmail.com>
2026-09-22 13:41:03 +0800

    done

--- app/src/test/java/com/example/courtscheduler/quarter2/ConsoleScheduler.java
+++ app/src/test/java/com/example/courtscheduler/quarter2/ConsoleScheduler.java
@@ -1,4 +1,31 @@
 package com.example.courtscheduler.quarter2;
 
+
+
+import java.util.Scanner;
 public class ConsoleScheduler {
+
+    public static void main(String[] args) {
+        LoginComponent();
+    }
+
+    public static void LoginComponent() {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+
+        System.out.println("\n--- Login Details Captured ---");
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
+        scanner.close();
+    }
 }
