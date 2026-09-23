bcd0b8cf33c53ca17d8fcdffa988d2a70fd53017
nep1009 <phenalmonte@gmail.com>
2026-09-23 20:15:02 +0800

    SCHDULER1/2

--- app/src/test/java/com/example/courtscheduler/quarter2/ConsoleScheduler.java
+++ app/src/test/java/com/example/courtscheduler/quarter2/ConsoleScheduler.java
@@ -1,30 +1,32 @@
 package com.example.courtscheduler.quarter2;
-
-
-
 import java.util.Scanner;
+
 public class ConsoleScheduler {
 
     public static void main(String[] args) {
-        LoginComponent();
+        SchedulerComponent();
     }
-
-    public static void LoginComponent() {
+    public static void SchedulerComponent() {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter User ID: ");
+        String userId = scanner.nextLine();
 
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
+        System.out.print("Enter start time: ");
+        String startTime = scanner.nextLine();
 
+        System.out.print("Enter end time: ");
+        String endTime = scanner.nextLine();
 
-        System.out.print("Enter password: ");
-        String password = scanner.nextLine();
+        System.out.print("Enter date: ");
+        String date = scanner.nextLine();
 
-
-        System.out.println("\n--- Login Details Captured ---");
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
+        System.out.println("\n--- Schedule Details Captured ---");
+        System.out.println("User ID: " + userId);
+        System.out.println("Start Time: " + startTime);
+        System.out.println("End Time: " + endTime);
+        System.out.println("Date: " + date);
 
         scanner.close();
     }
